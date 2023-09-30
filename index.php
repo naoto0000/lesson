1e26a2c6cd132bd8fdaef089673fac496ad230b0
なおと <fukuoka0583@gmail.com>
2023-09-30 15:56:39 +0900

    最初のコミット

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<?php 
+
+ini_set('log_errors','on');  //ログを取るか
+ini_set('error_log','php_error.log');  //ログの出力ファイルを指定
+
+$comment_array = array();
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AD5 lesson</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+
+</head>
+<body>
+
+<header>
+    <h1>社員一覧</h1>
+    <p>あいうえお</p>
+</header>
+    
+
+<main>
+    <table>
+        <tr>
+            <th>氏名</th>
+            <th>かな</th>
+            <th>性別</th>
+            <th>年齢</th>
+            <th>生年月日</th>
+        </tr>
+    </table>
+</main>
+</body>
+</html>
